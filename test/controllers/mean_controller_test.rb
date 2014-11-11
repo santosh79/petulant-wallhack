5e15340fcf7aaf07e136d8dc8428687a4251a5d8
Santosh Kumar <santosh79@gmail.com>
2014-11-11 14:58:03 -0500

    tests are green

--- test/controllers/mean_controller_test.rb
+++ test/controllers/mean_controller_test.rb
@@ -26,9 +26,28 @@ class MeanControllerTest < ActionController::TestCase
     assert_equal assigns(:mean), previous_mean
   end
 
-  test "should get update" do
-    post :update
+  test "update for the FIRST time" do
+    number = 100
+    session[:numbers_looked_at] = 0
+    post :update, :number => number
     assert_redirected_to mean_index_path
+    assert_equal session[:numbers_looked_at], 1
+    assert_equal session[:mean], number
   end
 
+  test "update AFTER the FIRST time" do
+    number = 100
+    numbers_looked_at = 10
+    cur_mean = 100
+    session[:numbers_looked_at] = numbers_looked_at
+    session[:mean] = cur_mean
+
+    cur_sum = cur_mean * numbers_looked_at
+
+    new_mean = (cur_sum + number) / (numbers_looked_at + 1)
+    post :update, :number => number
+    assert_redirected_to mean_index_path
+    assert_equal session[:numbers_looked_at], (numbers_looked_at + 1)
+    assert_equal session[:mean], new_mean
+  end
 end
